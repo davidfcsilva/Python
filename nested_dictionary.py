a05d27926175c36eac99ae89d4923c9fe33d7f6d
David Silva <davidfcsilva@gmail.com>
2022-05-18 10:49:29 +0100

    Create nested_dictionary.py

---
+++ nested_dictionary.py
@@ -0,0 +1,34 @@
+{
+  key: [List],
+  key2: {Dict},
+}
+
+visited_cities = {
+  "France": "Paris",
+  "Germany": "Berlin",
+} 
+
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"],
+  "Germany": ["Berlin", "Hamburg", "Stuttgard"],
+}
+
+cities_visited = {
+  "France": {
+    "Paris": "Visited 2 times",
+    "Lille": "Visited 1 time",
+    "Dijon": "Visited 4 times",},
+  "Germany": {
+    "Berlin": "Visited 1 time",
+    "Hamburg": "Visited 3 times",
+    "Stuttgard": "Visited 5 times",},
+}
+
+travel_log_v2 = {
+  "France": {"cities_visited_v2": ["Paris", "Lille", "Dijon"], "total_visits": 5},
+  "Germany": {"cities_visited_v2": ["Berlin", "Hamburg", "Stuttgard"], "total_visits": 5},
+}
+
+print(visited_cities)
+print(travel_log)
+print(cities_visited)
